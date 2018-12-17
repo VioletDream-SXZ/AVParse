469332b5d1472bfb7246275496337a91bfab04ce
VioletDream-SXZ <33800677+VioletDream-SXZ@users.noreply.github.com>
2018-12-17 17:36:38 +0800

    添加头文件types.h定义
    添加头文件types.h定义

---
+++ linux/types.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef GY_TYPES_H
+#define GY_TYPES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdint.h>
+
+#include <iostream>
+
+#endif // GY_TYPES_H
